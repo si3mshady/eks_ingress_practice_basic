4bed9dd1b9986382d99c34c28984e99b1cfe61e0
Elliott Arnold <elliottarnold@Elliotts-Air.attlocal.net>
2022-10-06 00:54:12 -0500

    save point

---
+++ Dockerfile
@@ -0,0 +1,38 @@
+# pull the official base image
+FROM node:alpine
+# set working direction
+WORKDIR /app
+# add `/app/node_modules/.bin` to $PATH
+ENV PATH /app/node_modules/.bin:$PATH
+# install application dependencies
+COPY package.json ./
+RUN npm i
+# add app
+COPY . ./
+
+EXPOSE 3000
+# start app
+CMD ["npm", "start"]
+
+# $ eksctl utils associate-iam-oidc-provider --region=us-east-1 --cluster=elliotteks --approve
+
+# eksctl create iamserviceaccount \
+#   --cluster=elliotteks \
+#   --namespace=kube-system \
+#   --name=aws-load-balancer-controller \
+#   --role-name "AmazonEKSLoadBalancerControllerRole" \
+#   --attach-policy-arn=arn:aws:iam::780988366548:policy/AWSLoadBalancerControllerIAMPolicy \
+#   --approve
+
+
+  
+
+
+#   helm upgrade aws-load-balancer-controller eks/aws-load-balancer-controller \
+#   -n kube-system \
+#   --set clusterName=elliotteks \
+#   --set serviceAccount.create=false \
+#   --set serviceAccount.name=aws-load-balancer-controller \
+#   --set region=us-east-1\
+#   --set vpcId=vpc-0737dd2cdac3788f2 \
+#   --set image.repository=602401143452.dkr.ecr.us-east-1.amazonaws.com/amazon/aws-load-balancer-controller
--- src/App.js
+++ src/App.js
@@ -13,13 +13,10 @@ function App() {
 
   return (
     <>
-
-   
     <div className='masterContainer'>
 
     <div className='masterContainer_top' >
 
-    
 
     <Clock style={{color:color}} className='masterContainer_top_clock' format={'HH:mm:ss'} ticking={true} timezone={timezone} />
     <p style={{color:"white"}}   className='masterContainer_top_p'>{timezone}</p>
@@ -27,40 +24,21 @@ function App() {
   
     <div className='masterContainer_bottom' >
 
-   
-
     <Stack className="masterContainer_bottom_buttons" direction="row" spacing={2}>
-
-    
-    
       <Button onClick={() => {setTimezone("America/New_York")}} variant="contained" >America/New_York</Button>
-      <Button onClick={() => {setTimezone('US/Pacific')}} variant="contained" color="success">
-      US/Pacific
-      </Button>
-      <Button onClick={() => {setTimezone('US/Central')}} variant="contained" color="error">
-      US/Central
-      </Button>
+      <Button onClick={() => {setTimezone('US/Pacific')}} variant="contained" color="success">US/Pacific</Button>
+      <Button onClick={() => {setTimezone('US/Central')}} variant="contained" color="error">US/Central</Button>
     </Stack>
 
     <Stack className="masterContainer_bottom_buttons" direction="row" spacing={2}>
     
-      <Button   style={{color:"red"}}  onClick={() => {setColor("red")}}  >RED</Button>
-      <Button  style={{color:"purple"}}  onClick={() => {setColor('purple')}}  >
-      PURPLE
-      </Button>
-      <Button  style={{color:"blue"}}  onClick={() => {setColor('blue')}}  >
-      BLUE
-      </Button>
+      <Button  style={{color:"red"}}  onClick={() => {setColor("red")}}>RED</Button>
+      <Button  style={{color:"purple"}}  onClick={() => {setColor('purple')}}> PURPLE</Button>
+      <Button  style={{color:"blue"}}  onClick={() => {setColor('blue')}}>BLUE</Button>
     </Stack>
-
-
-      
-
 
     </div>
     
-    
-
      </div>
 
      </>
